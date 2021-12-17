d34ce7b60292192c66d89a40529ed0a83a4c9f80
adityabhanu <adityabhanu08@gmail.com>
2021-12-17 16:15:19 +0530

    Max of all subarray of size K

---
+++ Arrays/arr06.py
@@ -0,0 +1,20 @@
+# Find max of all subarrays of size K
+# subarrays of size K => always think of sliding window approach once
+
+arr = [8, 5, 10, 7, 9, 4, 15, 12, 90, 13]
+K = 4
+
+
+def maxSubArr(arr, K):
+    maxArr = []
+    for i in range(0, len(arr)-K+1):
+        maxVal = arr[i]
+        for j in range(i+1, i+K):
+            if maxVal < arr[j]:
+                maxVal = arr[j]
+        maxArr.append(maxVal)
+    return maxArr
+
+
+maxSubArrayList = maxSubArr(arr, K)
+print(maxSubArrayList)
