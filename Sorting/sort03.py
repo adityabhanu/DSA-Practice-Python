61bbe904b6bad7914d1e83697df2efc464bc2e5a
adityabhanu <adityabhanu08@gmail.com>
2021-12-17 12:59:40 +0530

    Insertion Sort

--- Sorting/sort03.py
+++ Sorting/sort03.py
@@ -1,2 +1,22 @@
 # Insertion Sort
 # insert an element in a sorted array without removing ascending property
+
+arr = [2, 6, 4, 0, 1]
+
+
+def insertionSort(arr, n):
+    for i in range(1, n):
+        valueToSort = arr[i]
+
+        j = i-1
+
+        while j >= 0 and arr[j] > valueToSort:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = valueToSort
+
+    return arr
+
+
+sortedArr = insertionSort(arr, len(arr))
+print(sortedArr)
